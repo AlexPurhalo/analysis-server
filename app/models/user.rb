bb44d9a4b4d5191712828ffc19c39a2cb5799de9
alexpurhalo <alexpurhalo@gmail.com>
2016-08-23 20:08:00 +0300

    acces-token-generating-for-user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,21 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  after_create :update_access_token!
+
+  validates :email, presence: true
+
+  private
+  def update_access_token!
+    self.access_token = generate_access_token
+    save
+  end
+
+  def generate_access_token
+    loop do
+      token = "#{self.id}:#{Devise.friendly_token}"
+      break token unless User.where(access_token: token).first
+    end
+  end
 end
